aa6a0ffeceb9d37c7cd2dffe0e34d5531b04103e
sosag <sosag@GAEL>
2025-02-12 16:51:49 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace distancia_de_2_puntos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("inserta el valor de la coordenada de x2");
+            double x2=  double.Parse(Console.ReadLine());
+            System.Console.WriteLine("inserta el valor de la coordenada de x1");
+            double x1= double.Parse(Console.ReadLine());
+            double Z=x2 - x1;
+            double R1 = Math.Pow(Z,2);
+            System.Console.WriteLine("inserte el valor de la coordenada de y2");
+            double Y2= double.Parse(Console.ReadLine());
+            System.Console.WriteLine("ingrese el valor de la coordenada de y1");
+            double Y1=double.Parse(Console.ReadLine());
+            double W=Y1 - Y2;
+            double R2= Math.Pow(W,2);
+            double D= R1 + R2;  
+            double S=Math.Sqrt(D);
+            System.Console.WriteLine("El resultado es:"+S);
+            Console.ReadKey();
+        }
+    }
+}
